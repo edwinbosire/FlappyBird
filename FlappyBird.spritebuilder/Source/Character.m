8db41e7e14a01763bccf653b896f2d84d7bf0d5f
Edwin Bosire <dev@edwinb.co.uk>
2015-07-15 18:12:36 +0100

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/Character.m
+++ FlappyBird.spritebuilder/Source/Character.m
@@ -13,7 +13,7 @@
 
 - (void)didLoadFromCCB
 {
-    self.position = ccp(115, 250);
+    self.position = ccp(115, 150);
     self.zOrder = DrawingOrderHero;
     self.physicsBody.collisionType = @"character";
 }
